d5e2eb74d176820b93639a16682f8f9070cf04d0
Argo Roots <argo@roots.ee>
2015-06-25 15:35:57 +0300

    Get services from Entu; Create Fleep tasks

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -2,7 +2,8 @@ var PAGE_URL       = 'http://c3sregistration.studyitin.ee/'
 var API_URL        = 'https://entu.keeleressursid.ee/api2/'
 var API_USER       = 711
 var API_KEY        = 's2SuDSy9Bx5Qowj492ThZSKgZ8yCL7B1qBW2D11qeBF9KivEDCkM8LBvlPoa0Ddb'
-var FLEEP_HOOK_URL = 'https://fleep.io/hook/abypGACaReaTyC6PGvw1SQ'
+var FLEEP_HOOK_URL = 'https://fleep.io/chat/iMYo7cQRSimxY0w9ieqQCQ'
+// var FLEEP_HOOK_URL = 'https://fleep.io/hook/abypGACaReaTyC6PGvw1SQ'
 
 function cl(d) {
     console.log(d)
@@ -48,19 +49,75 @@ angular.module('ekrkApp', ['ngRoute', 'ngResource'])
 
 // START
     .controller('startCtrl', ['$scope', '$http', function($scope, $http) {
+        $scope.services = []
+
+        $http({
+                method : 'GET',
+                url    : API_URL + 'entity',
+                params   : getSignedData(API_USER, API_KEY, {
+                    definition: 'service'
+                })
+            })
+            .success(function(data) {
+                for(i in data.result) {
+                    $http({
+                            method : 'GET',
+                            url    : API_URL + 'entity-' + data.result[i].id,
+                            params   : getSignedData(API_USER, API_KEY, {})
+                        })
+                        .success(function(data) {
+                            var v = {
+                                id: data.result.id,
+                                fleep: [],
+                                checked: false
+                            }
+                            if(data.result.properties.name.values) v.name = data.result.properties.name.values[0].value
+                            if(data.result.properties.url.values) v.url = data.result.properties.url.values[0].value
+                            if(data.result.properties.description.values) v.description = data.result.properties.description.values[0].value
+                            if(data.result.properties.fleep.values) {
+                                for(f in data.result.properties.fleep.values) {
+                                    v.fleep.push(data.result.properties.fleep.values[f].value)
+                                }
+                            }
+                            $scope.services.push(v)
+                        })
+                        .error(function(data) {
+                            cl(data.error)
+                        })
+                }
+            })
+            .error(function(data) {
+                cl(data.error)
+            })
+
+
+
+
         $scope.checkData = function() {
             if(!$scope.application) return false
-            if(!$scope.application.forename || !$scope.application.surname || !$scope.application.email || !$scope.application.access) return false
+            if(!$scope.application.forename || !$scope.application.surname || !$scope.application.email || !$scope.services) return false
 
-            return $scope.application.access.entu || $scope.application.access.metashare || $scope.application.access.gitlab || $scope.application.access.hosting || $scope.application.access.www
+            var services = []
+            for(i in $scope.services) {
+                if($scope.services[i].checked) return true
+            }
+
+            return false
         }
 
         $scope.sendLink = function() {
-            if(!$scope.application.forename) return
-            if(!$scope.application.surname) return
-            if(!$scope.application.email) return
+            // if(!$scope.application.forename) return
+            // if(!$scope.application.surname) return
+            // if(!$scope.application.email) return
 
             $scope.sending = true
+
+            var services = []
+            for(i in $scope.services) {
+                if($scope.services[i].checked) services.push($scope.services[i].id)
+            }
+            cl(services.join(','))
+
             $http({
                     method : 'POST',
                     url    : API_URL + 'entity-682',
@@ -69,12 +126,7 @@ angular.module('ekrkApp', ['ngRoute', 'ngResource'])
                         'person-forename': $scope.application.forename,
                         'person-surname': $scope.application.surname,
                         'person-email': $scope.application.email,
-                        'person-company': $scope.application.company,
-                        'person-access-entu': $scope.application.access.entu,
-                        'person-access-metashare': $scope.application.access.metashare,
-                        'person-access-gitlab': $scope.application.access.gitlab,
-                        'person-access-hosting': $scope.application.access.hosting,
-                        'person-access-www': $scope.application.access.www,
+                        'person-company': $scope.application.company
                     })
                 })
                 .success(function(data) {
@@ -87,11 +139,31 @@ angular.module('ekrkApp', ['ngRoute', 'ngResource'])
                             })
                         })
                         .success(function(data) {
+                            var tasks = []
+                            for(i in $scope.services) {
+                                if($scope.services[i].checked) {
+                                    $http({
+                                        method : 'PUT',
+                                        url    : API_URL + 'entity-' + $scope.id,
+                                        data   : getSignedData(API_USER, API_KEY, {
+                                            'person-service': $scope.services[i].id
+                                        })
+                                    })
+                                    .success(function(data) {
+                                        cl(data)
+                                    })
+                                    .error(function(data) {
+                                        cl(data.error)
+                                        $scope.sending = false
+                                    })
+                                    tasks.push($scope.services[i].fleep)
+                                }
+                            }
                             $http({
                                     method : 'POST',
                                     url    : FLEEP_HOOK_URL,
                                     data   : {
-                                        'message': 'https://entu.keeleressursid.ee/entity/person/' + $scope.id
+                                        'message': 'https://entu.keeleressursid.ee/entity/person/' + $scope.id + tasks.join('\n/taskto @')
                                     }
                                 })
                                 .success(function(data) {
